3b7221c019eecf49f41e115977eede1a853d869b
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2019-07-29 17:59:51 +0200

    alert and console.log informations were added

---
+++ main.js
@@ -0,0 +1,6 @@
+alert('Herzlich willkommen auf meiner Homepage');
+console.log('Michał');
+console.log('32');
+const name = 'Michał';
+const age = 32;
+console.log(`Mein Name ist ${name} und ich bin ${age} jare alt`);
