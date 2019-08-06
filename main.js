77942d3918c67c04eb6a2ff15880b0b480f08bb4
Michał Adamczyk <michal.robert.adamczyk@gmail.com>
2019-08-06 19:20:09 +0200

    hamburger done

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log('32');
 const name = 'Michał';
 const age = 32;
 console.log(`Mein Name ist ${name} und ich bin ${age} jare alt`);
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
